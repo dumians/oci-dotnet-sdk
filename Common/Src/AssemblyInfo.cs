1f0c922b23d0d43844c6c7b6679ab020b2b2332c
johannes dumitru <johannes@almarchitect.net>
2020-08-07 23:16:07 +0200

    Update Nuget packages Nlog Xunit Add gitignore, icon.png, assemblyinfo

---
+++ Common/Src/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OCI.DotNetSDK.Common")]
+[assembly: AssemblyDescription("The common package provides supporting classes and methods used by service packages.It includes HTTP request/response (de)serialization, request signing, JSON parsing, and other helper function")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("58369776-0903-41c6-ae5b-0cce93bebdd3")]
